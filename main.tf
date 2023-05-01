c48baf86df319ba97b4cd064f3937b9460714c3d
pankaj <pankajsingh63@gmail.com>
2023-05-01 21:39:51 +0530

    Updated the main file and added few tags in it

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "myVM1" {
   instance_type = "t2.micro"
   tags = {
     Name = "Example-01"
+    PatchGroup= "Group1"
+    Environment = "Dev"
+    CreatedOn = "01-May-2023"
   }
 }
 
@@ -12,11 +15,20 @@ resource "aws_instance" "myVM2" {
   instance_type = "t2.micro"
   tags = {
     Name = "Example-02"
+    PatchGroup= "Group1"
+    Environment = "Dev"
+    CreatedOn = "01-May-2023"
   }
 }
 
 resource "aws_s3_bucket" "myBucket" {
   bucket = "terraform-practest"
+  tags = {
+    Name = "S3_buckets"
+    PatchGroup= "N/A"
+    Environment = "Dev"
+    CreatedOn = "01-May-2023"
+  }
 }
 
 data "aws_vpc" "default_VPC" {
